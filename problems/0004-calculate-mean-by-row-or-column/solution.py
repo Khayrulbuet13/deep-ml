37f2333145de55860e50aae76c2e6e277e036392
Khayrulbuet13 <67819338+Khayrulbuet13@users.noreply.github.com>
2026-09-22 20:35:14 +0000

    Update: Calculate Mean by Row or Column

--- problems/0004-calculate-mean-by-row-or-column/solution.py
+++ problems/0004-calculate-mean-by-row-or-column/solution.py
@@ -8,5 +8,4 @@ def calculate_matrix_mean(matrix, mode: str) -> torch.Tensor:
     """
     a_t = torch.as_tensor(matrix, dtype=torch.float)
     # Your implementation here
-    
     return a_t.mean(dim=0 if mode=="column" else 1)
